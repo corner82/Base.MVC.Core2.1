dd4ace8e6710ebb70e2ce93c77612c120ddeea36
corner82 <311corner82@gmail.com>
2018-07-20 14:39:28 +0300

    esksik dosyalar eklendi

---
+++ Base.Core/Entities/Log/ExceptionHandlingLogModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Base.Core.Entities.Log
+{
+    public class ExceptionHandlingLogModel
+    {
+        public string Host { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string SessionID { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ Base.Core/Entities/Log/PageAccessLogModel.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Base.Core.Entities.Log
+{
+    public class PageAccessLogModel
+    {
+        public string UserName { get; set; }
+        public string Action { get; set; }
+        public string Controller { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string UserAgent { get; set; }
+        public string UserIP { get; set; }
+        public string Method { get; set; }
+        public string SessionID { get; set; }
+        public string UserToken { get; set; }
+        public string UserPublicKey { get; set; }
+        public string UserPrivateKey { get; set; }
+    }
+}
