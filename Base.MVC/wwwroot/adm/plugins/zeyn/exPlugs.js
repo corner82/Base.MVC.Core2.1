fef0596f2dfc1f0d74eea880aa084102bafc3926
corner82 <311corner82@gmail.com>
2018-07-30 14:34:49 +0300

    js debug kapatıldı

--- Base.MVC/wwwroot/adm/plugins/zeyn/exPlugs.js
+++ Base.MVC/wwwroot/adm/plugins/zeyn/exPlugs.js
@@ -341,17 +341,17 @@
             var self = this;
             if ($("#langCode").length > 0) {
                 if ($("#langCode").val() != null && $("#langCode").val() != '') {
-                    alert('lang code bulundu2');
+                    //alert('lang code bulundu2');
                     self.options.langDefault = $("#langCode").val();
                 }
 
             } else if (Cookies && Cookies.length > 0) {
                 if (Cookies.get('langCookie') != null && Cookies.get('langCookie') != '') {
-                    alert('lang code bulundu1');
+                    //alert('lang code bulundu1');
                     self.options.langDefault = Cookies.get('langCookie');
                 }
             } else {
-                alert('lang code bulundu3');
+                //alert('lang code bulundu3');
                 self.options.langDefault = 'tr';
             }
 
@@ -376,7 +376,7 @@
                      //alert(window.lang.translate('Brand Sales Units'));
                      window.lang.change(self.options.langDefault);
                  }, 4000);*/
-            alert(self.options.langDefault);
+            //alert(self.options.langDefault);
             window.lang.change(self.options.langDefault);
         },
 
