7e4f47e66b45de89146ee8c37cf0671881592891
hgul <hediyegul@gmail.com>
2018-08-06 11:55:08 +0300

    customer purchase plan tab üzerinde çalışıldı.

--- Base.MVC/wwwroot/adm/dist/js/pages/customer/customer.js
+++ Base.MVC/wwwroot/adm/dist/js/pages/customer/customer.js
@@ -22,6 +22,7 @@
     * @since 02/08/2016
     */
     $('#customerInfoForm').validationEngine();
+    $('#customerPurchaseForm').validationEngine();
  
 
     /* Geçici data */
@@ -31,26 +32,22 @@
         {
             text: "Seçiniz...",
             value: 1,
-            selected: true,
-            index: 0
+            selected: true
         },
         {
             text: "South Africa",
             value: 2,
-            selected: false,
-            index: 1
+            selected: false
         },
         {
             text: "Turkey",
             value: 3,
-            selected: false,
-            index: 2
+            selected: false
         },
         {
             text: "Germany",
             value: 4,
-            selected: false,
-            index: 3
+            selected: false
         }
     ];
 
@@ -467,6 +464,7 @@
     })
     ajaxACLResources_totalemployees.ajaxCallWidget('call');
 
+
     $("#loading-image-annuelrevenue").loadImager();
     $("#loading-image-annuelrevenue").loadImager('appendImage');
 
@@ -517,6 +515,216 @@
         },
     })
     ajaxACLResources_annuelrevenue.ajaxCallWidget('call');
+
+    /* Purchase Plans loading services */
+
+
+    $("#loading-image-lastpurchasedbrand").loadImager();
+    $("#loading-image-lastpurchasedbrand").loadImager('appendImage');
+
+    var ajaxACLResources_lastpurchasedbrand = $('#loading-image-lastpurchasedbrand').ajaxCallWidget({
+        proxy: 'https://jsonplaceholder.typicode.com/todos/',
+        data: {
+            url: '1'
+            //pk: $("#pk").val()
+        }
+
+    });
+
+    ajaxACLResources_lastpurchasedbrand.ajaxCallWidget({
+        onError: function (event, textStatus, errorThrown) {
+
+            dm.dangerMessage({
+                onShown: function () {
+                    $('#loading-image-lastpurchasedbrand').loadImager('removeLoadImage');
+                }
+            });
+            dm.dangerMessage('show', 'servis Bulunamamıştır...', 'Servis  bulunamamıştır...');
+        },
+        onSuccess: function (event, data) {
+            //var data = $.parseJSON(cbdata);
+
+            $('#dropdownLastPurchaseBrand').ddslick({
+                //height: 150,
+                data: cbdata,
+                width: '100%',
+
+                onSelected: function (selectedData) {
+                    if (selectedData.selectedData.value > 0) {
+
+                    }
+                }
+            });
+
+            $("#loading-image-lastpurchasedbrand").loadImager('removeLoadImage');
+        },
+        onErrorDataNull: function (event, data) {
+            console.log("Error : " + event + " -data :" + data);
+            dm.dangerMessage({
+                onShown: function () {
+                    $('#loading-image-lastpurchasedbrand').loadImager('removeLoadImage');
+                }
+            });
+            dm.dangerMessage('show', 'Rol Bulunamamıştır...', 'Rol  bulunamamıştır...');
+        },
+    })
+    ajaxACLResources_lastpurchasedbrand.ajaxCallWidget('call');
+
+
+    $("#loading-image-purchasedecision").loadImager();
+    $("#loading-image-purchasedecision").loadImager('appendImage');
+
+    var ajaxACLResources_purchasedecision = $('#loading-image-purchasedecision').ajaxCallWidget({
+        proxy: 'https://jsonplaceholder.typicode.com/todos/',
+        data: {
+            url: '1'
+            //pk: $("#pk").val()
+        }
+
+    });
+
+    ajaxACLResources_purchasedecision.ajaxCallWidget({
+        onError: function (event, textStatus, errorThrown) {
+
+            dm.dangerMessage({
+                onShown: function () {
+                    $('#loading-image-purchasedecision').loadImager('removeLoadImage');
+                }
+            });
+            dm.dangerMessage('show', 'servis Bulunamamıştır...', 'Servis  bulunamamıştır...');
+        },
+        onSuccess: function (event, data) {
+            //var data = $.parseJSON(cbdata);
+
+            $('#dropdownPurchaseDecision').ddslick({
+                //height: 150,
+                data: cbdata,
+                width: '100%',
+
+                onSelected: function (selectedData) {
+                    if (selectedData.selectedData.value > 0) {
+
+                    }
+                }
+            });
+
+            $("#loading-image-purchasedecision").loadImager('removeLoadImage');
+        },
+        onErrorDataNull: function (event, data) {
+            console.log("Error : " + event + " -data :" + data);
+            dm.dangerMessage({
+                onShown: function () {
+                    $('#loading-image-purchasedecision').loadImager('removeLoadImage');
+                }
+            });
+            dm.dangerMessage('show', 'Rol Bulunamamıştır...', 'Rol  bulunamamıştır...');
+        },
+    })
+    ajaxACLResources_purchasedecision.ajaxCallWidget('call');
+
+
+    $("#loading-image-dateofpurchaseplan").loadImager();
+    $("#loading-image-dateofpurchaseplan").loadImager('appendImage');
+
+    var ajaxACLResources_dateofpurchaseplan = $('#loading-image-dateofpurchaseplan').ajaxCallWidget({
+        proxy: 'https://jsonplaceholder.typicode.com/todos/',
+        data: {
+            url: '1'
+            //pk: $("#pk").val()
+        }
+
+    });
+
+    ajaxACLResources_dateofpurchaseplan.ajaxCallWidget({
+        onError: function (event, textStatus, errorThrown) {
+
+            dm.dangerMessage({
+                onShown: function () {
+                    $('#loading-image-dateofpurchaseplan').loadImager('removeLoadImage');
+                }
+            });
+            dm.dangerMessage('show', 'servis Bulunamamıştır...', 'Servis  bulunamamıştır...');
+        },
+        onSuccess: function (event, data) {
+            //var data = $.parseJSON(cbdata);
+
+            $('#dropdownDateofPurchasePlan').ddslick({
+                //height: 150,
+                data: cbdata,
+                width: '100%',
+
+                onSelected: function (selectedData) {
+                    if (selectedData.selectedData.value > 0) {
+
+                    }
+                }
+            });
+
+            $("#loading-image-dateofpurchaseplan").loadImager('removeLoadImage');
+        },
+        onErrorDataNull: function (event, data) {
+            console.log("Error : " + event + " -data :" + data);
+            dm.dangerMessage({
+                onShown: function () {
+                    $('#loading-image-dateofpurchaseplan').loadImager('removeLoadImage');
+                }
+            });
+            dm.dangerMessage('show', 'Rol Bulunamamıştır...', 'Rol  bulunamamıştır...');
+        },
+    })
+    ajaxACLResources_dateofpurchaseplan.ajaxCallWidget('call');
+
+
+    $("#loading-image-numberofvehiclestopurchase").loadImager();
+    $("#loading-image-numberofvehiclestopurchase").loadImager('appendImage');
+
+    var ajaxACLResources_numberofvehiclestopurchase = $('#loading-image-numberofvehiclestopurchase').ajaxCallWidget({
+        proxy: 'https://jsonplaceholder.typicode.com/todos/',
+        data: {
+            url: '1'
+            //pk: $("#pk").val()
+        }
+
+    });
+
+    ajaxACLResources_numberofvehiclestopurchase.ajaxCallWidget({
+        onError: function (event, textStatus, errorThrown) {
+
+            dm.dangerMessage({
+                onShown: function () {
+                    $('#loading-image-numberofvehiclestopurchase').loadImager('removeLoadImage');
+                }
+            });
+            dm.dangerMessage('show', 'servis Bulunamamıştır...', 'Servis  bulunamamıştır...');
+        },
+        onSuccess: function (event, data) {
+            //var data = $.parseJSON(cbdata);
+
+            $('#dropdownNumberofVehiclestoP').ddslick({
+                //height: 150,
+                data: cbdata,
+                width: '100%',
+
+                onSelected: function (selectedData) {
+                    if (selectedData.selectedData.value > 0) {
+
+                    }
+                }
+            });
+
+            $("#loading-image-numberofvehiclestopurchase").loadImager('removeLoadImage');
+        },
+        onErrorDataNull: function (event, data) {
+            console.log("Error : " + event + " -data :" + data);
+            dm.dangerMessage({
+                onShown: function () {
+                    $('#loading-image-numberofvehiclestopurchase').loadImager('removeLoadImage');
+                }
+            });
+            dm.dangerMessage('show', 'Rol Bulunamamıştır...', 'Rol  bulunamamıştır...');
+        },
+    })
+    ajaxACLResources_numberofvehiclestopurchase.ajaxCallWidget('call');
 
 
     /* devexgrid */
@@ -1143,32 +1351,45 @@
 
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         var target = $(e.target).attr("href") // activated tab
+        if (target == "#tab_1") {
+            
+           
+            //alert("#tab_1");
+        }
         if (target == "#tab_2") {
             // grid refresh olması gerektiği için kullanıldı.
             $(gridContainer_contactaddress).dxDataGrid("updateDimensions");
             $(gridContainer_contactphone).dxDataGrid("updateDimensions");
+            //alert("#tab_2");
         }
-    });
-
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        var target = $(e.target).attr("href") // activated tab
+        if (target == "#tab_3") {
+            //Müşteri seçili ise
+            //PurchasePlan görüntüle
+            //alert("#tab_3");
+        }
         if (target == "#tab_4") {
             // grid refresh olması gerektiği için kullanıldı.
             $(gridContainer_contactperson).dxDataGrid("updateDimensions");
+            //alert("#tab_4");
         }
-    });
-
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        var target = $(e.target).attr("href") // activated tab
         if (target == "#tab_5") {
             // grid refresh olması gerektiği için kullanıldı.
             $(gridContainer_activity).dxDataGrid("updateDimensions");
+            //alert("#tab_5");
         }
     });
+
+
     $('#registration-datepicker').datepicker({
         //autoclose: true,
         locale: 'en',
         format : 'yyyy/mm/dd'
+    });
+
+    $('#lastpurchase-datepicker').datepicker({
+        //autoclose: true,
+        locale: 'en',
+        format: 'yyyy/mm/dd'
     });
 
 
@@ -1311,15 +1532,15 @@
 
         $('#customerInfoForm').validationEngine('hide');
         
-        $('#dropdownCountry').ddslick('select', { index: 0 });
-        $('#dropdownCity').ddslick('select', { index: 0 });
-        $('#dropdownSector').ddslick('select', { index: 0 });
-        $('#dropdownSegment').ddslick('select', { index: 0 });
-        $('#dropdownReliabilityRate').ddslick('select', { index: 0 });
-        $('#dropdownCustomerGroup').ddslick('select', { index: 0 });
-        $('#dropdownTotalVehicles').ddslick('select', { index: 0 });
-        $('#dropdownTotalEmployees').ddslick('select', { index: 0 });
-        $('#dropdownAnnuelRevenue').ddslick('select', { index: 0 });
+        $('#dropdownCountry').ddslick('select', { index: String(0) });
+        $('#dropdownCity').ddslick('select', { index: String(0) });
+        $('#dropdownSector').ddslick('select', { index: String(0) });
+        $('#dropdownSegment').ddslick('select', { index: String(0) });
+        $('#dropdownReliabilityRate').ddslick('select', { index: String(0) });
+        $('#dropdownCustomerGroup').ddslick('select', { index: String(0) });
+        $('#dropdownTotalVehicles').ddslick('select', { index: String(0) });
+        $('#dropdownTotalEmployees').ddslick('select', { index: String(0) });
+        $('#dropdownAnnuelRevenue').ddslick('select', { index: String(0) });
         
         loaderInsertBlock.loadImager('removeLoadImage');
 
@@ -1341,13 +1562,13 @@
 
         document.getElementById("registration-datepicker").value = Date();
         
-        $('#dropdownCountry').ddslick('select', { index: 1 });
+        $('#dropdownCountry').ddslick('select', { index: 3 });
         $('#dropdownCity').ddslick('select', { index: 2 });
-        $('#dropdownSector').ddslick('select', { index: 1 });
+        $('#dropdownSector').ddslick('select', { index: 3 });
         $('#dropdownSegment').ddslick('select', { index: 2 });
-        $('#dropdownReliabilityRate').ddslick('select', { index: 1 });
+        $('#dropdownReliabilityRate').ddslick('select', { index: 3 });
         $('#dropdownCustomerGroup').ddslick('select', { index: 2 });
-        $('#dropdownTotalVehicles').ddslick('select', { index: 1 });
+        $('#dropdownTotalVehicles').ddslick('select', { index: 3 });
         $('#dropdownTotalEmployees').ddslick('select', { index: 2 });
         $('#dropdownAnnuelRevenue').ddslick('select', { index: 2 });
         
@@ -1358,5 +1579,156 @@
         loaderInsertBlock.loadImager('removeLoadImage');
         return false;
     }
+
+
+
+    window.insertCustomerPurchaseWrapper = function (e) {
+        e.preventDefault();
+
+        if ($("#customerPurchaseForm").validationEngine('validate')) {
+
+            insertCustomerPurchase();
+        }
+        return false;
+    }
+    /**
+ * insert CustomerPurchase
+ * @returns {undefined}
+ * @since 06/08/2018
+ */
+
+    window.insertCustomerPurchase = function () {
+
+        var loaderInsertBlock = $("#loading-image-cstPurchase").loadImager();
+        loaderInsertBlock.loadImager('appendImage');
+
+        //Customer_id alınacak
+        var cst_id = 1;
+        var cst_lastpurchase = $('#lastpurchase-datepicker').val();
+        var cst_purchaselastupdate = $('#txt-cst-purchaselastupdate').val();
+
+        var ddData_lastPurchaseBrand = $('#dropdownLastPurchaseBrand').data('ddslick')
+        var lastPurchaseBrand_id = ddData_lastPurchaseBrand.selectedData.value;
+
+        var ddData_purchaseDecision = $('#dropdownPurchaseDecision').data('ddslick')
+        var purchaseDecision_id = ddData_purchaseDecision.selectedData.value;
+
+        var ddData_dateofPurchasePlan = $('#dropdownDateofPurchasePlan').data('ddslick')
+        var dateofPurchasePlan_id = ddData_dateofPurchasePlan.selectedData.value;
+
+        var ddData_numberofVehiclestoP = $('#dropdownNumberofVehiclestoP').data('ddslick')
+        var numberofVehiclestoP_id = ddData_numberofVehiclestoP.selectedData.value;
+
+
+        var aj = $(window).ajaxCall({
+            proxy: 'https://proxy.codebase_v2.com/SlimProxyBoot.php',
+            data: {
+                url: 'pkInsert_sysCustomerPurchase',
+                cst_id: cst_id,
+                cst_lastpurchase: cst_lastpurchase,
+                cst_purchaselastupdate: cst_purchaselastupdate,
+                lastPurchaseBrand_id: lastPurchaseBrand_id,
+                purchaseDecision_id: purchaseDecision_id,
+                dateofPurchasePlan_id: dateofPurchasePlan_id,
+                numberofVehiclestoP_id: numberofVehiclestoP_id,
+                pk: $("#pk").val()
+            }
+        })
+        aj.ajaxCall({
+            onError: function (event, textStatus, errorThrown) {
+                dm.dangerMessage('resetOnShown');
+                dm.dangerMessage('show', 'Müşteri Yatırım Planı Ekleme İşlemi Başarısız...',
+                    'Müşteri Ekleme İşlemi Başarısız..., sistem yöneticisi ile temasa geçiniz... ')
+                console.error('"pkInsert_sysCustomerInfo" servis hatası->' + textStatus);
+                loaderInsertBlock.loadImager('removeLoadImage');
+            },
+            onSuccess: function (event, data) {
+                console.log(data);
+                var data = data;
+                sm.successMessage({
+                    onShown: function (event, data) {
+                        $('#customerPurchaseForm')[0].reset();
+
+                        loaderInsertBlock.loadImager('removeLoadImage');
+                        
+                    }
+                });
+                sm.successMessage('show', 'Müşteri Yatırım Planı Kayıt İşlemi Başarılı...',
+                    'Müşteri kayıt işlemini gerçekleştirdiniz... ',
+                    data);
+                loaderInsertBlock.loadImager('removeLoadImage');
+
+            },
+            onErrorDataNull: function (event, data) {
+                dm.dangerMessage('resetOnShown');
+                dm.dangerMessage('show', 'Müşteri Yatırım Planı Kayıt İşlemi Başarısız...',
+                    'Müşteri Yatırım Planı kayıt işlemi başarısız, sistem yöneticisi ile temasa geçiniz... ');
+                console.error('"pkInsert_sysCustomerPurchase" servis datası boştur!!');
+                loaderInsertBlock.loadImager('removeLoadImage');
+            },
+            onErrorMessage: function (event, data) {
+                dm.dangerMessage('resetOnShown');
+                dm.dangerMessage('show', 'Müşteri Yatırım Planı Kayıt İşlemi Başarısız...',
+                    'Müşteri Yatırım Planı kayıt işlemi başarısız, sistem yöneticisi ile temasa geçiniz... ');
+                console.error('"pkInsert_sysCustomerPurchase" servis datası boştur!!');
+                loaderInsertBlock.loadImager('removeLoadImage');
+            },
+            onError23503: function (event, data) {
+                dm.dangerMessage('Error23503');
+                loaderInsertBlock.loadImager('removeLoadImage');
+            },
+            onError23505: function (event, data) {
+                dm.dangerMessage({
+                    onShown: function (event, data) {
+                        $('#customerPurchaseForm')[0].reset();
+                        loaderInsertBlock.loadImager('removeLoadImage');
+                    }
+                });
+                dm.dangerMessage('show', 'Kayıt İşlemi Başarısız...',
+                    'Aynı isim ile Müşteri  kaydı yapılmıştır, yeni bir Müşteri kaydı deneyiniz... ');
+                loaderInsertBlock.loadImager('removeLoadImage');
+            }
+        })
+        aj.ajaxCall('call');
+    }
+
+    window.resetCustomerPurchaseForm = function () {
+
+        var loaderInsertBlock = $("#loading-image-cstPurchase").loadImager();
+        loaderInsertBlock.loadImager('appendImage');
+
+        $('#customerPurchaseForm').validationEngine('hide');
+
+        $('#dropdownLastPurchaseBrand').ddslick('select', { index: String(0) });
+        $('#dropdownPurchaseDecision').ddslick('select', { index: String(0) });
+        $('#dropdownDateofPurchasePlan').ddslick('select', { index: String(0) });
+        $('#dropdownNumberofVehiclestoP').ddslick('select', { index: String(0) });
+  
+        loaderInsertBlock.loadImager('removeLoadImage');
+
+        return false;
+    }
+
+    window.fillCustomerPuchaseForm = function (data) {
+        //örnektir...
+        var loaderInsertBlock = $("#loading-image-cstPurchase").loadImager();
+        loaderInsertBlock.loadImager('appendImage');
+
+        document.getElementById("lastpurchase-datepicker").value = Date();
+        document.getElementById("txt-cst-purchaselastupdate").value = Date();
+
+        $('#dropdownLastPurchaseBrand').ddslick('select', { index: 3 });
+        $('#dropdownPurchaseDecision').ddslick('select', { index: 2 });
+        $('#dropdownDateofPurchasePlan').ddslick('select', { index: 3 });
+        $('#dropdownNumberofVehiclestoP').ddslick('select', { index: 2 });
+
+        //document.getElementById("dropdownCity").SelectedIndex = 2; //data.cityId;
+        //document.getElementById("dropdownSector").value = data.sectorId;
+        //document.getElementById("dropdownSegment").value = data.segmentId;
+
+        loaderInsertBlock.loadImager('removeLoadImage');
+        return false;
+    }
+
 });
 
