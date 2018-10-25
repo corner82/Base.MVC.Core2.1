32437e7099fa1fb3ae397f81174452819a291f87
cseyrek <37661923+cseyrek@users.noreply.github.com>
2018-10-25 15:23:03 +0300

    Trucks to go servis düzenlemeleri

---
+++ Base.MVC/Models/HttpRequest/Truckstogo/Truckstogo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Base.MVC.Models.HttpRequest.Truckstogo
+{
+    public class Truckstogo
+    {
+        public string url { get; set; }
+        public int? id { get; set; }
+        public string stock_id { get; set; }
+        public string truckstogo_type_id { get; set; }
+        public string description { get; set; }
+        public string etd_date { get; set; }
+        public string pk { get; set; }
+        public string pkIdentity { get; set; }
+    }
+}
