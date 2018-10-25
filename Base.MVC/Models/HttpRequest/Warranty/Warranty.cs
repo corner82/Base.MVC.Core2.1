8f840a71ace707a7f0a980ec453af216ae16959a
cseyrek <37661923+cseyrek@users.noreply.github.com>
2018-10-25 08:57:33 +0300

    Factory garanti servis düzenlemeleri--> model id grid verisi düzeltilecek.

--- Base.MVC/Models/HttpRequest/Warranty/Warranty.cs
+++ Base.MVC/Models/HttpRequest/Warranty/Warranty.cs
@@ -8,7 +8,8 @@ namespace Base.MVC.Models.HttpRequest.Warranty
     public class Warranty
     {
         public string url { get; set; }
-        public string warranty_id { get; set; }
+        public int? id { get; set; }
+        public int? warranty_id { get; set; }
         public string vehicle_config_type_id { get; set; }
         public string months1_id { get; set; }
         public string mileages1_id { get; set; }
@@ -16,6 +17,10 @@ namespace Base.MVC.Models.HttpRequest.Warranty
         public string ismaintenance { get; set; }
         public string unique_code { get; set; }
         public string price_in_euros { get; set; }
+        public int? vehicle_gt_model_id { get; set; }
+        public int? vehicle_app_types_id { get; set; }
+        public int? is_factory { get; set; }
+        public int? vehicle_group_id { get; set; }
         public string pk { get; set; }
     }
 }
--- Base.MVC/Models/HttpRequest/Warranty/WarrantyName.cs
+++ Base.MVC/Models/HttpRequest/Warranty/WarrantyName.cs
@@ -8,6 +8,7 @@ namespace Base.MVC.Models.HttpRequest.Warranty
     public class WarrantyName
     {
         public string url { get; set; }
+        public int? id { get; set; }
         public string name { get; set; }
         public string vehicle_group_id { get; set; }
         public string pk { get; set; }
